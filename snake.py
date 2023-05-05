78c8254083d8921dc81714be9e30c974527b184c
Tanishh <88125649+TanishhBhajnik@users.noreply.github.com>
2023-05-05 21:12:55 +0530

    Add files via upload

---
+++ snake.py
@@ -0,0 +1,54 @@
+from turtle import Turtle
+
+snake_position = [(0, 0), (-20, 0), (-40, 0)]
+move_distance = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for position in snake_position:
+            self.add_segments(position)
+
+
+    def add_segments(self, position):
+        snake = Turtle(shape="square")
+        snake.color("white")
+        snake.penup()
+        snake.goto(position)
+        self.segments.append(snake)
+
+    def extend(self):
+        self.add_segments(self.segments[-1].position())
+
+
+    def move(self):
+        for seg_num in range (len(self.segments)-1, 0,-1 ):
+            x_cor = self.segments[seg_num - 1].xcor()
+            y_cor = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(x_cor, y_cor)
+        self.segments[0].forward(move_distance)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
